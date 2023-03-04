3e8291f0e51554d779c0acc44fac1007bd1dfa83
WashingtonDC <washingtonluismm@gmail.com>
2023-03-04 12:38:00 -0800

    add button class

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-import pygame
-import button
-
-#create display window
+import pygame, button
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Button Demo')
 
-#load button images
 start_img = pygame.image.load('start_btn.png').convert_alpha()
 exit_img = pygame.image.load('exit_btn.png').convert_alpha()
 
-#create button instances
 start_button = button.Button(100, 200, start_img, 0.8)
 exit_button = button.Button(450, 200, exit_img, 0.8)
 
-#game loop
 run = True
 while run:
 
@@ -27,9 +21,7 @@ while run:
 	if exit_button.draw(screen):
 		print('EXIT')
 
-	#event handler
 	for event in pygame.event.get():
-		#quit game
 		if event.type == pygame.QUIT:
 			run = False
 
